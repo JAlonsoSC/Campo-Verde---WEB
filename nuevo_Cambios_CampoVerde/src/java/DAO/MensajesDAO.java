35ddb238feaf7f1d6373823465682047277309f1
JAlonsoSC <144565344+JAlonsoSC@users.noreply.github.com>
2024-07-18 10:24:40 -0500

    Delete nuevo_Cambios_CampoVerde directory

--- nuevo_Cambios_CampoVerde/src/java/DAO/MensajesDAO.java
+++
@@ -1,9 +0,0 @@
-package DAO;
-
-import java.util.List;
-import modelos.Mensajes;
-
-public interface MensajesDAO {
-    void enviarMensaje(Mensajes mensaje) throws Exception;
-    List<Mensajes> obtenerMensajesDisponibles(int emisorID, int receptorID) throws Exception;
-}
